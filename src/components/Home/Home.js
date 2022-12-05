60092ef94ba9a5a119e8b3cf2b74701b067d86c3
athulmekkoth <athulmekkoth21@gmail.com>
2022-12-05 16:04:19 +0530

    mb

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,12 +7,14 @@ export default function Home()
     return(
         <div className="body" >
             <div className="  text-white text-left py-20 px-16">
-            <h1 className="flex font-semibold text-purple-600">
+            <h1 className=" text-3xlmd:flex font-semibold text-purple-600 text-5xl">
   Hi ,There <span className="animate-waving-hand">👋🏻</span></h1>
-  <h1 className=" whitespace-nowrap text-4xl">I'm <span className="text-4xl text-violet-900 inline-block animate-bounce w-6 h-6 ...">ATHUL SURESH </span></h1>
+  <h1 className="text-3xl whitespace-nowrap md:whitespace-nowrap text-9xl">I'm <span className="text-3xl md:text-9xl text-violet-900 inline-block animate-bounce w-6 h-6 ...">ATHUL SURESH </span></h1>
         </div>
 
-        <img  className="" src={cd} alt="img" />
+        <img className="w-48 ml-20 mt-28 sm:w-1/4 rounded-2xl absolute right-16 top-56  "  src={cd} alt="img" />
+  
+        
             </div>
          
             
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar()
            
         <ul className="flex flex-row flex-nowrap justify-between ">  
         <h1 className="text-5xl px-4 py-3 text-stone-50  sm:text-slate-500">AS</h1>
-        <div className="text-2xl py-4 px-6   text-stone-50  sm:text-1xl">
+        <div className=" text-sm md:text-2xl py-4 px-6   text-stone-50  sm:text-1xl">
         <Link className="px-3 py-3" to='/'> Home</Link>
         <Link className="px-3"to='/projects'>Projects</Link>
         <Link className="px-3" to='/about'>About</Link>
